04db649e24256353c8f2cf3ef0a8c5f90b609ed2
Md. Rifat Islam Rizvi <rifatrizviofficial001@gmail.com>
2026-01-09 17:04:08 +0600

    Add task creation form and views for module-6

--- task_management/tasks/models.py
+++ task_management/tasks/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
+    
+    def __str__(self):
+        return self.name
 class Task(models.Model):
     project = models.ForeignKey(
         "Project",
