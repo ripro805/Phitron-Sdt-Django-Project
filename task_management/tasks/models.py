87504b14ce32c51496e6cfd3816218c4a537cb14
Md. Rifat Islam Rizvi <rifatrizviofficial001@gmail.com>
2026-01-10 23:50:02 +0600

    7.1 retriving data

--- task_management/tasks/models.py
+++ task_management/tasks/models.py
@@ -7,7 +7,12 @@ class Employee(models.Model):
     
     def __str__(self):
         return self.name
-class Task(models.Model):
+class Task(models.Model): 
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('COMPLETED', 'Completed')
+    ]
     project = models.ForeignKey(
         "Project",
         on_delete=models.CASCADE,
@@ -17,9 +22,14 @@ class Task(models.Model):
     title=models.CharField(max_length=200)
     description=models.TextField()
     due_date=models.DateField()
+    status = models.CharField(
+        max_length=15, choices=STATUS_CHOICES, default="PENDING")
     is_completed=models.BooleanField(default=False)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
 
 class TaskDetail(models.Model):
     HIGH='H'
@@ -37,9 +47,16 @@ class TaskDetail(models.Model):
        )
     assigned_to=models.CharField(max_length=100)
     priority=models.CharField(max_length=1, choices=PRIORITY_CHOICES,default=LOW)
+    notes = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return f"Details form Task {self.task.title}"
     
     
 class Project(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
     start_date = models.DateField()
     
+    
+    def __str__(self):
+        return self.name
--- task_management/tasks/views.py
+++ task_management/tasks/views.py
@@ -31,22 +31,17 @@ def create_task(request):
             #For Model Form
             form.save()
             return render(request, "task_form.html", {"form": form, "message": "Task Created Successfully"})
-            #For Django Form
-        #    data = form.cleaned_data
-        #    title=data.get('title')
-        #    description=data.get('description')
-        #    due_date=data.get('due_date')
-        #    assigned_to=data.get('assigned_to')
-           
-        #    task=Task.objects.create(
-        #        title=title,
-        #        description=description,
-        #        due_date=due_date,
-        #    )
-        #    #Assign employees to the task
-        #    for emp_id in assigned_to:
-        #        employee=Employee.objects.get(id=emp_id)
-        #        task.assigned_to.add(employee)
+       
         return HttpResponse("Task Created Successfully")   
     context={'form':form}
     return render(request, 'task_form.html', context)
+
+def view_tasks(request):
+    #retrive all tasks
+    tasks=Task.objects.all()
+    #retrive a specific tasks 
+    task_3=Task.objects.get(pk=3)
+    #Fetch first task
+    first_task=Task.objects.first()
+
+    return render(request,'show_tasks.html',{"tasks":tasks,"task_3":task_3,"first_task":first_task})
