78afed7c3927011c47419afe0f7dd6f2c285e78e
Md. Rifat Islam Rizvi <rifatrizviofficial001@gmail.com>
2026-01-19 16:11:58 +0600

    10.3 Customizing user creation form

--- task_management/users/views.py
+++ task_management/users/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
+from users.forms import RegisterForm
+from django.contrib.auth.models import User
+
+
 # Create your views here.
 def sign_up(request):
     if request.method == 'GET':
-        form = UserCreationForm()
+        form = RegisterForm()
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
             form.save()
+        #     username= form.cleaned_data.get('username')
+        #     password= form.cleaned_data.get('password1')
+        #     confirm_password= form.cleaned_data.get('password2')
+        #     if password==confirm_password:
+        #         User.objects.create_user(username=username,password=password)
+        #     else:
+        #         print("Password and Confirm Password do not match")
+        # else:
+        #     print("Form is not valid")  
+                      
+            
         
     return render(request, 'registration/register.html',{'form': form})
     
