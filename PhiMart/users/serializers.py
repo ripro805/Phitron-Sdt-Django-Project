34133d833335ad30f5b9ffe3e9fe8f04f6e429ad
Md. Rifat Islam Rizvi <rifatrizviofficial001@gmail.com>
2026-02-24 05:14:58 +0600

    23.2 Registering User API

--- PhiMart/users/serializers.py
+++ PhiMart/users/serializers.py
@@ -1,17 +1,5 @@
-from rest_framework import serializers
-from .models import User
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'email', 'username', 'first_name', 'last_name']
-
-class UserCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'email', 'username', 'password', 'first_name', 'last_name']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = ['id',  'email', 'password','first_name', 'last_name', 'address', 'phone_number']
