245dcc08feb0e219aaedcecf7cbdaf5668237cdc
Md. Rifat Islam Rizvi <rifatrizviofficial001@gmail.com>
2026-02-24 04:38:46 +0600

    WIP: Save all local changes before merging module-22 into main

--- PhiMart/api/views.py
+++ PhiMart/api/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import redirect
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from django.urls import reverse
 
 @api_view(['GET'])
-def api_root_view(request):
-    # Optionally, you can return a JSON response with API links
+def api_home(request):
+    # Build useful API root links
+    api_links = {
+        "products": request.build_absolute_uri(reverse('products-list')),
+        "categories": request.build_absolute_uri(reverse('category-list')),
+        "carts": request.build_absolute_uri(reverse('carts-list')),
+        "cart-items": request.build_absolute_uri(reverse('cart-items-list')),
+    }
     return JsonResponse({
-        "message": "Welcome to the PhiMart API Root!",
-        "products": "/api/products/",
-        "categories": "/api/categories/",
-        "carts": "/api/carts/",
-        "cart-items": "/api/cart-items/",
-        "reviews": "/api/products/<product_id>/reviews/"
+        "message": "Welcome to the PhiMart API!",
+        "links": api_links
     })
