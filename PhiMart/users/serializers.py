10a9fcc2cfdc89bb0e11148b9973979d2cd720ee
Md. Rifat Islam Rizvi <rifatrizviofficial001@gmail.com>
2026-02-24 04:58:41 +0600

    Add UserSerializer and UserCreateSerializer for Djoser JWT user endpoints

---
+++ PhiMart/users/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'first_name', 'last_name']
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'password', 'first_name', 'last_name']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
