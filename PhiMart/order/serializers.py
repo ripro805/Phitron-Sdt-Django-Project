cd7366ca1436e02215c744a2340c5e1982e5e785
Md. Rifat Islam Rizvi <rifatrizviofficial001@gmail.com>
2026-02-24 02:54:35 +0600

    22.3 Serializer Nesting for Cart Items

--- PhiMart/order/serializers.py
+++ PhiMart/order/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Cart, CartItem, Order, OrderItem
 from users.models import User
+from product.serializers import ProductSerializer
 
 class UserDropDownField(serializers.PrimaryKeyRelatedField):
     def get_queryset(self):
@@ -9,21 +10,44 @@ class UserDropDownField(serializers.PrimaryKeyRelatedField):
     def display_value(self, instance):
         return instance.email
 
-class CartSerializer(serializers.ModelSerializer):
-    user = UserDropDownField(queryset=User.objects.all())
+
+
     class Meta:
         model = Cart
-        fields = '__all__'
+        fields = ['id', 'user', 'items']
     def validate_user(self, value):
         if Cart.objects.filter(user=value).exists():
             raise serializers.ValidationError("This user already has a cart.")
         return value
 
 class CartItemSerializer(serializers.ModelSerializer):
+    product_price = serializers.SerializerMethodField()
+
     class Meta:
         model = CartItem
-        fields = '__all__'
+        fields = ['id', 'cart', 'product_price', 'quantity']
 
+    def get_product_price(self, obj):
+        return obj.product.price if obj.product else None
+class CartSerializer(serializers.ModelSerializer):
+    user = UserDropDownField(queryset=User.objects.all())
+    items = CartItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'items', 'total_price']
+
+    def get_total_price(self, obj):
+        return sum(
+            (item.product.price if item.product else 0) * item.quantity
+            for item in obj.items.all()
+        )
+
+    def validate_user(self, value):
+        if Cart.objects.filter(user=value).exists():
+            raise serializers.ValidationError("This user already has a cart.")
+        return value
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
