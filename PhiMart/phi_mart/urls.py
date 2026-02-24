e7adbe3ba48d4baaab7b4c387ac56b8cb538e32f
Md. Rifat Islam Rizvi <rifatrizviofficial001@gmail.com>
2026-02-24 06:18:00 +0600

    Commit changes in workspace

--- PhiMart/phi_mart/urls.py
+++ PhiMart/phi_mart/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
 ]
 
 if settings.DEBUG:
